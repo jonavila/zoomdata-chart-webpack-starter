2ea991745af84f0746b580843662f69525dfeb15
Jonathan Avila <jonathan@zoomdata.com>
2018-09-19 09:24:06 -0400

    remove @babel/preset-react

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,7 @@
 module.exports = function(api) {
   api.cache.using(() => process.env.NODE_ENV === 'development');
 
-  const presets = [
-    ['@babel/preset-env', { modules: false }],
-    '@babel/preset-react',
-  ];
+  const presets = [['@babel/preset-env', { modules: false }]];
   const plugins = [
     ['@babel/plugin-proposal-class-properties', { loose: true }],
     '@babel/plugin-proposal-object-rest-spread',
