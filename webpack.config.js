5dbfc6b6a11fa0f084f11daab9110577c6844c0f
Jonathan Avila <jonathan@zoomdata.com>
2018-10-30 09:16:32 -0400

    Small config fixes

--- webpack.config.js
+++ webpack.config.js
@@ -37,16 +37,6 @@ const productionConfig = merge([
   },
   parts.clean(PATHS.build),
   parts.minifyJavaScript(),
-  parts.minifyCSS({
-    options: {
-      discardComments: {
-        removeAll: true,
-      },
-      // Run cssnano in safe mode to avoid
-      // potentially unsafe transformations.
-      parser: require('postcss-safe-parser'),
-    },
-  }),
 ]);
 
 const developmentConfig = merge([
